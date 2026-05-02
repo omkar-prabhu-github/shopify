0d2dfdbfc1017977d2e5adef2475a9c88fd44abe
Omkar Prabhu <omkarshanbhag123@gmail.com>
2026-05-02 20:27:28 +0530

    Fix Express 5 catch-all route syntax

--- backend/server.js
+++ backend/server.js
@@ -88,7 +88,7 @@ if (process.env.NODE_ENV === 'production') {
   const distPath = path.join(__dirname, '../dist');
   app.use(express.static(distPath));
 
-  app.get('*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 } else {
